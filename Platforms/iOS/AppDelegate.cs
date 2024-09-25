8addee78b17d49457de31421e8c21cca9e412e83
Gnidator5000 <Protoplast1221@gmail.com>
2024-09-25 19:43:41 +0200

    Dodaj pliki projektów.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RzutKostka
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
